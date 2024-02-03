c95cf5aa2d7d1eb726691f2d92d39f68ede45bb5
bbbii <hyeongjin1326@gmail.com>
2024-02-03 15:24:26 +0900

    rename: [bbbii] 맵 생성 버튼 Header로 이동 [MQF-9]

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,7 +1,19 @@
 import React, { ReactNode } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link as ReactRouterLink } from "react-router-dom";
-import { Link as ChakraLink, Spacer } from "@chakra-ui/react";
+import {
+  AspectRatio,
+  ButtonGroup,
+  Link as ChakraLink,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Spacer,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { Box, Button, Flex, Heading, Image } from "@chakra-ui/react";
 
 import { ColorModeSwitcher } from "../../ColorModeSwitcher";
@@ -10,8 +22,41 @@ type HeaderProps = {
   children?: ReactNode;
 };
 
+type ButtonOptions = {
+  url: string;
+  title: string;
+  label: string;
+};
+
+const buttonOptions: ButtonOptions[] = [
+  {
+    url: "/img/chino.jpg",
+    title: "music-quiz",
+    label: "노래 퀴즈",
+  },
+  {
+    url: "/img/fuyu.jpg",
+    title: "image-quiz",
+    label: "사진 퀴즈",
+  },
+];
+
 const Header: React.FC<HeaderProps> = ({ children }) => {
   const navigate = useNavigate();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleCreateMapButtonClick = () => {
+    onOpen();
+  };
+
+  const handleMapButtonClick = (title: string) => {
+    if (title === "music-quiz") {
+      navigate("/music-map-register");
+    } else if (title === "image-quiz") {
+      alert("준비중입니다");
+      // navigate("/image-map-register");
+    }
+  };
 
   const goToLoginPage = () => {
     navigate("/login");
@@ -29,9 +74,32 @@ const Header: React.FC<HeaderProps> = ({ children }) => {
         </Heading>
       </Box>
       <Spacer />
-      <Button colorScheme="green" onClick={goToLoginPage}>
-        로그인
-      </Button>
+      <ButtonGroup>
+        <Button onClick={handleCreateMapButtonClick}>맵 만들기</Button>
+        <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose} isCentered>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>맵 타입을 선택하세요</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Flex justifyContent="center" gap="10%">
+                {buttonOptions.map((button: ButtonOptions) => (
+                  <Button key={button.title} onClick={() => handleMapButtonClick(button.title)} w="100em" h="auto">
+                    <AspectRatio ratio={1} width="100%" height="100%">
+                      <Image src={button.url} alt={button.label} objectFit="cover" />
+                    </AspectRatio>
+                    {button.label}
+                  </Button>
+                ))}
+              </Flex>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+
+        <Button colorScheme="green" onClick={goToLoginPage}>
+          로그인
+        </Button>
+      </ButtonGroup>
       <ColorModeSwitcher />
       {children}
     </Flex>
--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -1,56 +1,7 @@
 import React from "react";
-import {
-  AspectRatio,
-  Box,
-  Button,
-  Flex,
-  Image,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalHeader,
-  ModalOverlay,
-  useDisclosure,
-} from "@chakra-ui/react";
-import { useNavigate } from "react-router-dom";
-
-interface ButtonOptions {
-  url: string;
-  title: string;
-  label: string;
-}
-
-const buttonOptions: ButtonOptions[] = [
-  {
-    url: "/img/chino.jpg",
-    title: "music-quiz",
-    label: "노래 퀴즈",
-  },
-  {
-    url: "/img/fuyu.jpg",
-    title: "image-quiz",
-    label: "사진 퀴즈",
-  },
-];
+import { Box, Button, Flex } from "@chakra-ui/react";
 
 const MainPage: React.FC = () => {
-  const navigate = useNavigate();
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  const handleCreateMapButtonClick = () => {
-    onOpen();
-  };
-
-  const handleMapButtonClick = (title: string) => {
-    if (title === "music-quiz") {
-      navigate("/music-map-register");
-    } else if (title === "image-quiz") {
-      alert("준비중입니다");
-      // navigate("/image-map-register");
-    }
-  };
-
   return (
     <Flex direction="column" align="center">
       <Box
@@ -62,27 +13,6 @@ const MainPage: React.FC = () => {
         <Button>싱글 플레이</Button>
         <Button>멀티 플레이</Button>
       </Box>
-      <Button onClick={handleCreateMapButtonClick}>맵 만들기</Button>
-
-      <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose} isCentered>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>맵 타입을 선택하세요</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Flex justifyContent="center" gap="10%">
-              {buttonOptions.map((button: ButtonOptions) => (
-                <Button key={button.title} onClick={() => handleMapButtonClick(button.title)} w="100em" h="auto">
-                  <AspectRatio ratio={1} width="100%" height="100%">
-                    <Image src={button.url} alt={button.label} objectFit="cover" />
-                  </AspectRatio>
-                  {button.label}
-                </Button>
-              ))}
-            </Flex>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
     </Flex>
   );
 };
